60159335a76a82692362129c19b240b45b009c3b
fano <aafano19@gmail.com>
2019-08-10 01:57:46 +0300

    last version

---
+++ the_gossip_project3/app/models/gossip.rb
@@ -0,0 +1,11 @@
+class Gossip < ApplicationRecord
+	validates :title, 
+	presence: true,
+	length: { in: 3..30 }
+	validates :content,
+	presence: true
+	belongs_to :user
+	has_many :relations 
+	has_many :tags,through: :relations
+	has_many :comments
+end
